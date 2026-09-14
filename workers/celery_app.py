0841f37d4053c253470fce3747bc8535b00fb9af
sm1le-devops <gvh423699@gmail.com>
2026-09-14 18:22:03 +0200

    update celery_app

--- workers/celery_app.py
+++ workers/celery_app.py
@@ -7,7 +7,8 @@ result_backend = os.getenv("REDIS_URL_CACHE", "redis://redis_broker:6379/1")
 celery = Celery(
     "fastapi_app",
     broker=broker_url,
-    backend=result_backend
+    backend=result_backend,
+    include=["workers.celery_tasks"]  
 )
 
 celery.conf.update(
