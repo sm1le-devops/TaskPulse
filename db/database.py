c1de650a1b546caee07e1f898770d1f3d2238267
sm1le-devops <gvh423699@gmail.com>
2026-09-14 18:56:17 +0200

    update database condition on cache=shared

--- db/database.py
+++ db/database.py
@@ -1,16 +1,29 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-
+from sqlalchemy.pool import StaticPool
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
 
-connect_args = {"check_same_thread": False} if "sqlite" in SQLALCHEMY_DATABASE_URL else {}
+if "sqlite" in SQLALCHEMY_DATABASE_URL:
+    connect_args = {"check_same_thread": False}
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args=connect_args
-)
+    if ":memory:" in SQLALCHEMY_DATABASE_URL:
+        if "cache=shared" not in SQLALCHEMY_DATABASE_URL:
+            SQLALCHEMY_DATABASE_URL += "?cache=shared"
+        engine = create_engine(
+            SQLALCHEMY_DATABASE_URL, 
+            connect_args=connect_args, 
+            poolclass=StaticPool
+        )
+    else:
+        engine = create_engine(
+            SQLALCHEMY_DATABASE_URL, 
+            connect_args=connect_args
+        )
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
